72dcd7084101010c4ddafe23053d999062349b4e
Armaan-2007 <armaanchandani7@gmail.com>
2025-09-30 12:38:54 +0530

    Create day41a.c
    Count characters in a string without using built-in length functions in c

---
+++ day41a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int count = 0, i = 0;
+    printf("Enter a string: ");
+    gets(str); 
+    while(str[i] != '\0') {
+        count++;
+        i++;
+    }
+
+    printf("Number of characters in the string: %d\n", count);
+
+    return 0;
+}
