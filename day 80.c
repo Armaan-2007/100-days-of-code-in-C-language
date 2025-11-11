c131762490ddca4705d2bfa80adcfeed5cb23145
Armaan-2007 <armaanchandani7@gmail.com>
2025-11-11 23:42:30 +0530

    Create day 80.c

---
+++ day 80.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    FILE *fp;
+    struct Student s;
+    int n, i;
+    fp = fopen("students.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening file for writing!\n");
+        exit(1);
+    }
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        printf("\nEnter details for student %d\n", i + 1);
+        printf("Name: ");
+        scanf("%s", s.name);
+        printf("Roll Number: ");
+        scanf("%d", &s.roll);
+        printf("Marks: ");
+        scanf("%f", &s.marks);
+
+        fprintf(fp, "%s %d %.2f\n", s.name, s.roll, s.marks);
+    }
+
+    fclose(fp);
+    fp = fopen("students.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file for reading!\n");
+        exit(1);
+    }
+
+    printf("\n--- Student Records from File ---\n");
+    while (fscanf(fp, "%s %d %f", s.name, &s.roll, &s.marks) == 3) {
+        printf("Name: %s\tRoll: %d\tMarks: %.2f\n", s.name, s.roll, s.marks);
+    }
+
+    fclose(fp);
+    return 0;
+}
