5b22c03bc47c23f3243fb9e5083fa5f095f7bf5e
Armaan-2007 <armaanchandani7@gmail.com>
2025-11-22 14:34:06 +0530

    Add Student structure and print function

---
+++ day 94.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Define a structure
+struct Student {
+    int roll;
+    char name[50];
+    float marks;
+};
+void printStudent(struct Student s) {
+    printf("Roll Number : %d\n", s.roll);
+    printf("Name        : %s\n", s.name);
+    printf("Marks       : %.2f\n", s.marks);
+}
+
+int main() {
+    
+    struct Student st = {101, "Rahul", 92.5};
+    printStudent(st);
+
+    return 0;
+}
